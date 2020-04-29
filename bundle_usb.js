5471feb79032e2a4428c1d7b3769f5cf65938a49
julientaverne <38781186+julientaverne@users.noreply.github.com>
2020-04-29 17:51:56 +0200

    update

--- bundle_usb.js
+++ bundle_usb.js
@@ -3876,7 +3876,7 @@ exports.getFileName = function getFileName(calling_file) {
 exports.getRoot = function getRoot(file) {
   var dir = dirname(file),
     prev;
-    var  existees =
+    function  existees (path) { 
     (fs.accessSync &&
       function(path) {
         try {
@@ -3887,7 +3887,7 @@ exports.getRoot = function getRoot(file) {
         return true;
       }) ||
     fs.existsSync ||
-    path.existsSync;
+    path.existsSync};
   while (true) {
     if (dir === '.') {
       // Avoids an infinite loop in rare cases, like the REPL
