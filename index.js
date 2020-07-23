80670e411fb76d326759eb162597f6ca4b07e764
julientaverne <38781186+julientaverne@users.noreply.github.com>
2020-07-23 17:24:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,104 +1,11 @@
-
-function requestDevice() {
-  /*
-  navigator.usb.requestDevice({ filters: [{ }] })
-    .then(showDevices)
-    .catch(usbError)
-  */
-  document.addEventListener('DOMContentLoaded', async () => {
-    console.log("julien");
-  let devices = await navigator.hid.getDevices();
-  devices.forEach(device => {
-    console.log('HID: ${device.productName}');
-  });
-});
-  
- 
-  
-  /*
-   navigator.hid.addEventListener('connect', async () => {
-  console.log('HID connected: ${device.productName}');
-});
-
-navigator.hid.addEventListener('disconnect', async () => {
-  console.log('HID disconnected: ${device.productName}');
-});
-*/
-
-}
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-  });
-
-for( let i = 0; i < 1000000000; i++)
-{} // This synchronous script is going to delay parsing of the DOM,
-   // so the DOMContentLoaded event is going to launch later.
-
-let requestButton = document.getElementById('request-hid-device');
-requestButton.addEventListener('click', async () => {
-  let device;
-  try {
-    device = await navigator.hid.requestDevice(
-     /*
-      { filters: [{
-        vendorId: 0xABCD,
-        productId: 0x1234,
-        usagePage: 0x0C,
-        usage: 0x01
-    }]}
-    */
-      { filters: [{ }] }
-    );
-  } catch (error) {
-    console.log('No device was selected 2.');
-  }
-
-  if (device !== undefined) {
-    console.log('HID: ${device.productName} 2');
+document.getElementById('arduinoButton').addEventListener('click', function () {
+  if (navigator.usb) {
+    talkToArduino();
+  } else {
+    console.log('WebUSB not supported.');
   }
 });
 
-
-
-
-
-var classes = {
-  [0x00]: "Unknown",
-  [0x01]: "Audio",
-  [0x02]: "Communications and CDC Control",
-  [0x03]: "Human Interface Device (HID)",
-  [0x05]: "Physical",
-  [0x06]: "Image",
-  [0x07]: "Printer",
-  [0x08]: "Mass Storage (MSD)",
-  [0x09]: "Hub",
-  [0x0A]: "CDC-Data",
-  [0x0B]: "Smart Card",
-  [0x0D]: "Content Security",
-  [0x0E]: "Video",
-  [0x0F]: "Personal Healthcare",
-  [0x10]: "Audio/Video Devices",
-  [0x11]: "Billboard Device",
-  [0xDC]: "Diagnostic Device",
-  [0xE0]: "Wireless Controller",
-  [0xEF]: "Miscellaneous",
-  [0xFE]: "Application Specific",
-  [0xFF]: "Vendor Specific",
-}
-
-function showDevices(d) {
-  console.log('devices:', d)
-  var info = document.getElementById('info')
-  info.innerHTML = `<ul>
-    <li>${d.manufacturerName} ${d.productName}</li>
-    <li>vendorId: 0x${d.vendorId.toString(16)}</li>
-    <li>vendorId JTA: ${d.vendorId}</li>
-    <li>class: ${classes[d.deviceClass]}</li>
-  </ul>`
-}
-
-function usbError(e) {
-  console.log('usb error:', e)
+function talkToArduino(){
+  console.log ('WebUSB OK! ');
 }
