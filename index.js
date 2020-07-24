73161414e2cfd7dd2e6f883bf0c11ad6abb1fbb3
julientaverne <38781186+julientaverne@users.noreply.github.com>
2020-07-24 17:33:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,17 @@
-/*
-document.getElementById('arduinoButton').addEventListener('click', function () {
-  if (chrome.hid) {
-    talkToArduino();
-    console.log('web HID not supported.');
+
+document.getElementById('butbut').addEventListener('click', function () {
+  if (chrome.usb) {
+    console.log('web USB supported.');
   } else {
     console.log('WebUSB not supported.');
   }
-});
-
-async function talkToArduino() {
-  console.log("GO");
-  try {
-    chrome.hid.getDevices()
-    .then(devices => {
-      console.log("Total devices: " + devices.length);
-      devices.forEach(device => {
-        console.log("Product name: " + device.productName + ", serial number " + device.serialNumber);
-      });
-    });
-  } catch (error) {
-    document.getElementById('target').innerHTML = "-->"+error;
-  }
-}
-*/
-
-const consentButton = document.getElementById('consent-button');
-
-const deviceFilters = [{ }];
-
-consentButton.addEventListener('click', async () => {
-  let device;
-
-  let devices = await navigator.hid.getDevices();
-  devices.forEach(device => {
-    console.log('HID: ${device.productName}');
+  console.log("01");
+  navigator.usb.getDevices().then(devices => {
+    console.log("02");
+  devices.map(device => {
+    console.log("03");
+    console.log(device.productName);      // "Arduino Micro"
+    console.log(device.manufacturerName); // "Arduino LLC"
   });
-  
-  /*
-  try {
-    const devices = await navigator.hid.requestDevice({ filters: deviceFilters });
-    device = devices[0];
-  } catch (error) {
-    console.warn('No device access granted', error);
-    return;
-  }
-  */
+})
 });
